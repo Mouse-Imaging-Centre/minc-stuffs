737af74b895049ce5c2ed98b4f68a74586ed31f1
jasonlerch <jason@phenogenomics.ca>
2012-11-05 20:01:31 -0500

    * extra options for compute_values_across_segmentation

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name="python-stuffs",
-      version='0.1',
+      version='0.1.1',
       scripts=["python/TFCE", 
 	       "python/smooth_vector", 
 	       "python/measure_xcorr", 
                "python/pmincaverage", 
                "python/minc_label_ops", 
-               "python/average_values_across_segmentation", 
+               "python/compute_values_across_segmentation", 
                "python/volumes_from_labels_only", 
                "python/voxel_vote"]
       )
