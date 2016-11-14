d43b7481529b95d2c51866f755f0ab2533a0bf9a
Ben Darwin <bcdarwin@gmail.com>
2016-11-14 13:59:34 -0500

    add `install_requires` to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import sys
 
 setup(name="python-stuffs",
       version='0.1.17',
+      install_requires=['numpy', 'scipy', 'pyminc'],
       scripts=["python/TFCE",
                "python/smooth_vector",
                "python/measure_xcorr",
