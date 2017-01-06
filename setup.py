4eb9d0307382443eea8451a97842c3324ad34b27
Matthijs van Eede <matthijsvaneede@gmail.com>
2017-01-06 18:00:19 -0500

    bumped version to 0.1.19, updated the NEWS

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import sys
 
 setup(name="python-stuffs",
-      version='0.1.18',
+      version='0.1.19',
       install_requires=['numpy', 'scipy', 'pyminc'],
       scripts=["python/TFCE",
                "python/smooth_vector",
